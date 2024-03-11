29227df033eadf359b14f9eefbcd67a872d0038f
Milan Mazik <mazik1990@hotmail.com>
2024-03-11 13:00:45 +0100

    client side auth refactored

--- frontend/Script/script.js
+++ frontend/Script/script.js
@@ -1,11 +1,24 @@
-let myToken = '';
 
-fetch('http://localhost:3000/api/token')
-  .then(data => data.json())
-  .then(data => {
-    myToken = data.access_token; // Itt mentjük el a választ a változóba
-    // Itt kezeld a választ
-  })
-  .catch(error => {
-    console.log('A kérés sikertelen', error);
-  });
+// localStorage setup
+let token = localStorage.getItem('token');
+
+// Get token if not exists
+if (!token === '' || token === null) {
+  fetch('http://localhost:3000/api/token')
+    .then(data => data.json())
+    .then(data => {
+      localStorage.setItem('token', data.access_token);
+      //save token
+      let myToken = data.access_token;
+      runBCWeb();
+    })
+    .catch(error => {
+      console.log('A kérés sikertelen', error);
+    });
+} else {
+  // A token használata a localStorage-ből
+  let myToken = token;
+  runBCWeb();
+}
+
+function runBCWeb() {}
