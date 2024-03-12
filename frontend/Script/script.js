13b9dd0782bc8267f56379694d987ea39632c00d
Milan Mazik <mazik1990@hotmail.com>
2024-03-12 14:40:10 +0100

    render sales Quotes

--- frontend/Script/script.js
+++ frontend/Script/script.js
@@ -1,13 +1,92 @@
 //pointers to html buttons
 const customerButton = document.querySelector(".js-customers-button");
-const itemsButton = document.querySelector("js-items-button");
-const salesQuotesButton = document.querySelector("js-salesQuotes-button");
+const itemsButton = document.querySelector(".js-items-button");
+const salesQuotesButton = document.querySelector(".js-salesQuotes-button");
 
 //pointers to html body areas
-const customerContainer = document.querySelector(".customersContainer");
+const contentContainer = document.querySelector(".contentContainer");
+//Connection datas
+const serverHost = window.location.hostname;
+const serverPort = window.location.port;
+const customersAPI = `http://${serverHost}:${serverPort}/api/customers`;
+const itemsAPI = `http://${serverHost}:${serverPort}/api/items`;
+const salesQuotesAPI = `http://${serverHost}:${serverPort}/api/salesQuotes`;
 
-const customersAPI = "http://localhost:3000/api/customers";
+//prepare sales Quotes data for rendering
+let salesQuotes = [];
+async function loadSalesQuotes() {
+  let data = await fetch(salesQuotesAPI);
+  let dataJson = await data.json();
+  salesQuotes = dataJson.value;
+}
 
+async function renderSalesQuotes() {
+  try {
+    if (salesQuotes.length === 0) {
+      await loadSalesQuotes();
+    }
+
+    contentContainer.innerHTML = "";
+    salesQuotes.forEach((salesQuote) => {
+      const salesQuoteElement = document.createElement("div");
+      salesQuoteElement.classList.add("salesQuote");
+
+      const salesQuoteInfo = `
+        
+        <h2>Quote Number: ${salesQuote.no}</h2>
+        <p>Document Type: ${salesQuote.documentType}</p>
+        <p>Document Date: ${salesQuote.documentDate}</p>
+        <p>Due Date: ${salesQuote.dueDate}</p>
+        <p>Posting Date: ${salesQuote.postingDate}</p>
+        <p>Sell To Customer Number: ${salesQuote.sellToCustomerNo}</p>
+      `;
+      salesQuoteElement.innerHTML = salesQuoteInfo;
+      contentContainer.appendChild(salesQuoteElement);
+    });
+  } catch (error) {
+    console.error("Error rendering salesQuotes data:", error);
+    contentContainer.innerHTML = `<p>Error loading salesQuotes: ${error.message}</p>`;
+  }
+}
+
+//prepare items data for rendering
+let items = [];
+async function loadItems() {
+  let data = await fetch(itemsAPI);
+  let dataJson = await data.json();
+  items = dataJson.value;
+}
+
+async function renderItems() {
+  try {
+    if (items.length === 0) {
+      await loadItems();
+    }
+
+    contentContainer.innerHTML = "";
+    items.forEach((item) => {
+      const itemElement = document.createElement("div");
+      itemElement.classList.add("item");
+
+      const itemInfo = `
+          <h2>${item.description}</h2>
+          <p>Item Number: ${item.no}</p>
+          <p>Description: ${item.description}${
+        item.description2 ? `, ${item.description2}` : ""
+      }</p>
+          <p>Unit Price: ${item.unitPrice}</p>
+        `;
+      itemElement.innerHTML = itemInfo;
+
+      contentContainer.appendChild(itemElement);
+    });
+  } catch (error) {
+    console.error("Error rendering items data:", error);
+    contentContainer.innerHTML = `<p>Error loading items: ${error.message}</p>`;
+  }
+}
+
+//prepare customers data for rendering
 let customers = [];
 async function loadCustomers() {
   let data = await fetch(customersAPI);
@@ -15,36 +94,35 @@ async function loadCustomers() {
   customers = dataJson.value;
 }
 
-function renderCustomers() {
-  if (customers.length === 0) {
-    loadCustomers()
-      .then(() => {
-        customerContainer.innerHTML = "";
-        customers.forEach((customer) => {
-          const customerElement = document.createElement("div");
-          customerElement.classList.add("customer");
+async function renderCustomers() {
+  try {
+    if (customers.length === 0) {
+      await loadCustomers();
+    }
 
-          const customerInfo = `
-              <h2>${customer.name}</h2>
-              <p>Customer Number: ${customer.no}</p>
-              <p>Address: ${customer.address}${
-            customer.address2 ? `, ${customer.address2}` : ""
-          }</p>
-              <p>City: ${customer.city}, Postcode: ${customer.postCode}</p>
-            `;
-          customerElement.innerHTML = customerInfo;
+    contentContainer.innerHTML = "";
+    customers.forEach((customer) => {
+      const customerElement = document.createElement("div");
+      customerElement.classList.add("customer");
 
-          customerContainer.appendChild(customerElement);
-        });
-      })
-      .catch((error) => {
-        console.error("Error fetching customer data:", error);
-        const customerContainer = document.querySelector(".customersContainer");
-        customerContainer.innerHTML = `<p>Error loading customers: ${error.message}</p>`;
-      });
-  } else {
-    customerContainer.innerHTML = ""; // Clear previous content
+      const customerInfo = `
+        <h2>${customer.name}</h2>
+        <p>Customer Number: ${customer.no}</p>
+        <p>Address: ${customer.address}${
+        customer.address2 ? `, ${customer.address2}` : ""
+      }</p>
+        <p>City: ${customer.city}, Postcode: ${customer.postCode}</p>
+      `;
+      customerElement.innerHTML = customerInfo;
+
+      contentContainer.appendChild(customerElement);
+    });
+  } catch (error) {
+    console.error("Error rendering customer data:", error);
+    contentContainer.innerHTML = `<p>Error loading customers: ${error.message}</p>`;
   }
 }
 
 customerButton.addEventListener("click", renderCustomers);
+itemsButton.addEventListener("click", renderItems);
+salesQuotesButton.addEventListener("click", renderSalesQuotes);
