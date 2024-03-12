c62e2538765c085867a982919abd9a365c63265d
Milan Mazik <mazik1990@hotmail.com>
2024-03-12 10:42:00 +0100

    server endpoints: Sales quotes headers and lines. debug: wait for access token through first API call

--- server/index2.js
+++ server/index2.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.static("frontend"));
 
 // BC tenant and Customers data
+const userName = "[REPLACE WITH YOUR USERNAME]";
+const companyName = "[REPLACE WITH YOUR COMPANY NAME]";
+const environment = "[REPLACE WITH YOUR ENVIRONMENT NAME]";
 const clientId = "[REPLACE WITH YOUR CLIENT ID]";
 const clientSecret = "[REPLACE WITH YOUR CLIENTSECRET]";
 const scope = "https://api.businesscentral.dynamics.com/.default";
@@ -31,13 +34,63 @@ async function getToken() {
 
 let myToken = "";
 
+async function waitForToken() {
+  while (myToken === "") {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // function for first API calls: waiting for token
+  }
+}
+
 //FUNCTIONS FOR RETRIEVE DATAS
+
+//Get Sales Lines
+async function getSalesLines() {
+  try {
+    const response = await axios.get(
+      `https://api.businesscentral.dynamics.com/v2.0/${tenantID}/SandBox/api/${companyName}/${userName}/v1.0/companies(${companyID})/salesLines?$filter=documentType eq 'Quote'`,
+      {
+        headers: {
+          Authorization: `Bearer ${myToken}`,
+        },
+      }
+    );
+
+    const salesLines = response.data;
+    console.log("Sales Lines:", salesLines);
+
+    return salesLines;
+  } catch (error) {
+    console.error("An error occurred when retrieving Sales Lines:", error);
+    throw error;
+  }
+}
+
+//Get Sales Quotes
+async function getSalesQuotes() {
+  try {
+    const response = await axios.get(
+      `https://api.businesscentral.dynamics.com/v2.0/${tenantID}/SandBox/api/${companyName}/${userName}/v1.0/companies(${companyID})/salesHeaders?$filter=documentType eq 'Quote'`,
+      {
+        headers: {
+          Authorization: `Bearer ${myToken}`,
+        },
+      }
+    );
+
+    const salesQuotes = response.data;
+    console.log("Sales Quotes:", salesQuotes);
+
+    return salesQuotes;
+  } catch (error) {
+    console.error("An error occurred when retrieving Sales Quotes:", error);
+    throw error;
+  }
+}
 
 //Get items
 async function getItems() {
   try {
     const response = await axios.get(
-      `https://api.businesscentral.dynamics.com/v2.0/${tenantID}/SandBox/api/dynixSoftwareKft/training/v1.0/companies(${companyID})/items`,
+      `https://api.businesscentral.dynamics.com/v2.0/${tenantID}/${environment}/api/${companyName}/${userName}/v1.0/companies(${companyID})/items`,
       {
         headers: {
           Authorization: `Bearer ${myToken}`,
@@ -59,7 +112,7 @@ async function getItems() {
 async function getCustomers() {
   try {
     const response = await axios.get(
-      `https://api.businesscentral.dynamics.com/v2.0/${tenantID}/SandBox/api/dynixSoftwareKft/training/v1.0/companies(${companyID})/customers`,
+      `https://api.businesscentral.dynamics.com/v2.0/${tenantID}/${environment}/api/${companyName}/${userName}/v1.0/companies(${companyID})/customers`,
       {
         headers: {
           Authorization: `Bearer ${myToken}`,
@@ -69,7 +122,6 @@ async function getCustomers() {
 
     const customers = response.data;
     console.log("Ügyfelek:", customers);
-
     return customers;
   } catch (error) {
     console.error("An error occurred when retrieving customers:", error);
@@ -78,6 +130,52 @@ async function getCustomers() {
 }
 
 //ENDPOINTS
+
+//Sales Lines
+app.get("/api/salesLines", async (req, res) => {
+  if (myToken === "") {
+    (async () => {
+      try {
+        myToken = await getToken();
+        console.log("token:", myToken);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    })();
+  }
+  await waitForToken();
+
+  try {
+    const salesLines = await getSalesLines();
+    res.json(salesLines);
+  } catch (error) {
+    console.log("Error at requesting Sales Lines data:", error);
+    res.status(500).send("Error fetching sales lines");
+  }
+});
+
+//Sales Quotes
+app.get("/api/salesQuotes", async (req, res) => {
+  if (myToken === "") {
+    (async () => {
+      try {
+        myToken = await getToken();
+        console.log("token:", myToken);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    })();
+  }
+  await waitForToken();
+
+  try {
+    const salesQuotes = await getSalesQuotes();
+    res.json(salesQuotes);
+  } catch (error) {
+    console.log("Error at requesting Sales Quotes data:", error);
+    res.status(500).send("Error fetching sales quotes");
+  }
+});
 
 //Items
 app.get("/api/items", async (req, res) => {
@@ -91,6 +189,8 @@ app.get("/api/items", async (req, res) => {
       }
     })();
   }
+  await waitForToken();
+
   try {
     const items = await getItems();
     res.json(items);
@@ -112,6 +212,8 @@ app.get("/api/customers", async (req, res) => {
       }
     })();
   }
+  await waitForToken();
+
   try {
     const customers = await getCustomers();
     res.json(customers);
