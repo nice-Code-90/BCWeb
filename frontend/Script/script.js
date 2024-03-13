626ed8831b250203c4081092e086349be9f816cb
Milan Mazik <mazik1990@hotmail.com>
2024-03-13 14:53:18 +0100

    render sales lines

--- frontend/Script/script.js
+++ frontend/Script/script.js
@@ -1,10 +1,13 @@
 //pointers to html buttons
-const customerButton = document.querySelector(".js-customers-button");
+const customersButton = document.querySelector(".js-customers-button");
 const itemsButton = document.querySelector(".js-items-button");
 const salesQuotesButton = document.querySelector(".js-salesQuotes-button");
 
+const salesLinesButton = document.querySelector(".js-sales-Lines-button");
+
 //pointers to html body areas
 const contentContainer = document.querySelector(".contentContainer");
+
 //Connection datas
 const serverHost = window.location.hostname;
 const serverPort = window.location.port;
@@ -13,91 +16,38 @@ const itemsAPI = `http://${serverHost}:${serverPort}/api/items`;
 const salesQuotesAPI = `http://${serverHost}:${serverPort}/api/salesQuotes`;
 const salesLinesAPI = `http://${serverHost}:${serverPort}/api/salesLines`;
 
-//prepare sales Quotes data for rendering
-let salesQuotes = [];
-async function loadSalesQuotes() {
-  let data = await fetch(salesQuotesAPI);
+//prepare customers data for rendering
+let customers = [];
+async function loadCustomers() {
+  let data = await fetch(customersAPI);
   let dataJson = await data.json();
-  salesQuotes = dataJson.value;
+  customers = dataJson.value;
 }
-
-// Load and render Sales Lines for the selected Sales Quote
-async function loadAndRenderSalesLines(quoteNumber) {
+async function renderCustomers() {
   try {
-    // Load Sales Lines for the selected Sales Quote
-    let data = await fetch(
-      `${salesLinesAPI}?filter=Document_No eq ${quoteNumber}`
-    );
-    let dataJson = await data.json();
-    const salesLines = dataJson.value;
-
-    // Render Sales Lines
-    renderSalesLines(salesLines);
-  } catch (error) {
-    console.error("Error loading salesLines data:", error);
-    contentContainer.innerHTML = `<p>Error loading salesLines: ${error.message}</p>`;
-  }
-}
-
-// Render Sales Lines
-function renderSalesLines(salesLines) {
-  contentContainer.innerHTML = "";
-  salesLines.forEach((salesLine) => {
-    const salesLineElement = document.createElement("div");
-    salesLineElement.classList.add("salesLine");
-
-    // Additional information for Sales Lines
-    const salesLineInfo = `
-        <h2>Sales Line Number: ${salesLine.lineNumber}</h2>
-        <p>Item Number: ${salesLine.itemNo}</p>
-        <p>Description: ${salesLine.description}</p>
-        <p>Unit Price: ${salesLine.unitPrice}</p>
-        <p>Quantity: ${salesLine.quantity}</p>
-      `;
-    salesLineElement.innerHTML = salesLineInfo;
-
-    contentContainer.appendChild(salesLineElement);
-  });
-}
-
-async function renderSalesQuotes() {
-  try {
-    if (salesQuotes.length === 0) {
-      await loadSalesQuotes();
+    if (customers.length === 0) {
+      await loadCustomers();
     }
 
     contentContainer.innerHTML = "";
-    salesQuotes.forEach((salesQuote) => {
-      const salesQuoteElement = document.createElement("div");
-      salesQuoteElement.classList.add("salesQuote");
+    customers.forEach((customer) => {
+      const customerElement = document.createElement("div");
+      customerElement.classList.add("customer");
 
-      // Hyperlink for Sales Lines
-      const salesLinesLink = document.createElement("a");
-      salesLinesLink.href = "#"; // Placeholder, actual href set later
-      salesLinesLink.textContent = `Quote Number: ${salesQuote.no}`;
-      salesLinesLink.addEventListener("click", () =>
-        loadAndRenderSalesLines(salesQuote.no)
-      );
-      salesQuoteElement.appendChild(salesLinesLink);
-
-      const salesQuoteInfo = `
-        
-        <h2>Quote Number: ${salesQuote.no}</h2>
-        <p>Document Type: ${salesQuote.documentType}</p>
-        <p>Document Date: ${salesQuote.documentDate}</p>
-        <p>Due Date: ${salesQuote.dueDate}</p>
-        <p>Posting Date: ${salesQuote.postingDate}</p>
-        <p>Sell To Customer Number: ${salesQuote.sellToCustomerNo}</p>
-        <p>Sell To Customer Name: ${salesQuote.sellToCustomerName}</p>
-        <p>Sell To Contact: ${salesQuote.selltoContact}</p>
-        <p>Amount: ${salesQuote.Amount}</p>
+      const customerInfo = `
+        <h2>${customer.name}</h2>
+        <p>Customer Number: ${customer.no}</p>
+        <p>Address: ${customer.address}${
+        customer.address2 ? `, ${customer.address2}` : ""
+      }</p>
+        <p>City: ${customer.city}, Postcode: ${customer.postCode}</p>
       `;
-      salesQuoteElement.innerHTML = salesQuoteInfo;
-      contentContainer.appendChild(salesQuoteElement);
+      customerElement.innerHTML = customerInfo;
+      contentContainer.appendChild(customerElement);
     });
   } catch (error) {
-    console.error("Error rendering salesQuotes data:", error);
-    contentContainer.innerHTML = `<p>Error loading salesQuotes: ${error.message}</p>`;
+    console.error("Error rendering customer data:", error);
+    contentContainer.innerHTML = `<p>Error loading customers: ${error.message}</p>`;
   }
 }
 
@@ -108,13 +58,11 @@ async function loadItems() {
   let dataJson = await data.json();
   items = dataJson.value;
 }
-
 async function renderItems() {
   try {
     if (items.length === 0) {
       await loadItems();
     }
-
     contentContainer.innerHTML = "";
     items.forEach((item) => {
       const itemElement = document.createElement("div");
@@ -138,43 +86,103 @@ async function renderItems() {
   }
 }
 
-//prepare customers data for rendering
-let customers = [];
-async function loadCustomers() {
-  let data = await fetch(customersAPI);
+//prepare sales Quotes data for rendering
+let salesQuotes = [];
+async function loadSalesQuotes() {
+  let data = await fetch(salesQuotesAPI);
   let dataJson = await data.json();
-  customers = dataJson.value;
+  salesQuotes = dataJson.value;
 }
-
-async function renderCustomers() {
+async function renderSalesQuotes() {
   try {
-    if (customers.length === 0) {
-      await loadCustomers();
+    if (salesQuotes.length === 0) {
+      await loadSalesQuotes();
     }
 
     contentContainer.innerHTML = "";
-    customers.forEach((customer) => {
-      const customerElement = document.createElement("div");
-      customerElement.classList.add("customer");
+    salesQuotes.forEach((salesQuote) => {
+      const salesQuoteElement = document.createElement("div");
+      salesQuoteElement.classList.add("salesQuote");
 
-      const customerInfo = `
-        <h2>${customer.name}</h2>
-        <p>Customer Number: ${customer.no}</p>
-        <p>Address: ${customer.address}${
-        customer.address2 ? `, ${customer.address2}` : ""
-      }</p>
-        <p>City: ${customer.city}, Postcode: ${customer.postCode}</p>
-      `;
-      customerElement.innerHTML = customerInfo;
+      let salesQuoteInfo = "";
 
-      contentContainer.appendChild(customerElement);
+      salesQuoteInfo += `<h2>Document No: ${salesQuote.no}</h2>`;
+      salesQuoteInfo += `<p>Document Date: ${salesQuote.documentDate}</p>`;
+      salesQuoteInfo += `<p>Due Date: ${salesQuote.dueDate}</p>`;
+      salesQuoteInfo += `<p>Posting Date: ${salesQuote.postingDate}</p>`;
+      salesQuoteInfo += `<p>Sell To Customer Number: ${salesQuote.sellToCustomerNo}</p>`;
+      salesQuoteInfo += `<p>Sell To Customer Name: ${salesQuote.sellToCustomerName}</p>`;
+      salesQuoteInfo += `<p>Sell To Contact: ${salesQuote.selltoContact}</p>`;
+      salesQuoteInfo += `<p>Amount: ${salesQuote.Amount}</p>`;
+
+      let salesLinesButton = document.createElement("button");
+      salesLinesButton.classList.add("js-sales-Lines-button");
+      salesLinesButton.textContent = "More Info";
+
+      salesLinesButton.addEventListener("click", async () => {
+        await renderSalesLines();
+      });
+      salesQuoteElement.appendChild(salesLinesButton);
+      salesQuoteElement.innerHTML += salesQuoteInfo;
+      contentContainer.appendChild(salesQuoteElement);
+
+      // A salesLinesButton-hoz tartozó eseménykezelő
+      salesLinesButton = salesQuoteElement.querySelector(
+        ".js-sales-Lines-button"
+      );
+      salesLinesButton.addEventListener("click", async () => {
+        await renderSalesLines(salesQuote.no);
+      });
     });
   } catch (error) {
-    console.error("Error rendering customer data:", error);
-    contentContainer.innerHTML = `<p>Error loading customers: ${error.message}</p>`;
+    // Csatoljuk a többi információt
+    console.error("Error rendering salesQuotes data:", error);
+    contentContainer.innerHTML = `<p>Error loading salesQuotes: ${error.message}</p>`;
+  }
+}
+// prepare Sales Lines data for rendering
+let salesLines = [];
+async function loadSalesLines() {
+  try {
+    let data = await fetch(salesLinesAPI);
+    let dataJson = await data.json();
+    salesLines = dataJson.value;
+  } catch (error) {
+    console.error("Error loading salesLines:", error);
+    contentContainer.innerHTML = `<p>Error loading salesLines: ${error.message}</p>`;
   }
 }
 
-customerButton.addEventListener("click", renderCustomers);
+async function renderSalesLines(salesQuoteNo) {
+  try {
+    if (salesLines.length === 0) {
+      await loadSalesLines();
+    }
+    contentContainer.innerHTML = "";
+    salesLines.forEach((salesLine) => {
+      if (salesLine.documentNo === salesQuoteNo) {
+        const salesLineElement = document.createElement("div");
+        salesLineElement.classList.add("salesLine");
+
+        // Additional information for Sales Lines
+        const salesLineInfo = `
+        <h2>Description: ${salesLine.description} </h2>
+        <p>Document No: ${salesLine.documentNo} </p>
+        <p>Line No: ${salesLine.lineNo} </p>
+        <p>quantity: ${salesLine.quantity} </p>
+        <p>type: ${salesLine.type} </p>
+        <p>unit price: ${salesLine.unitPrice}</p>
+      `;
+        salesLineElement.innerHTML = salesLineInfo;
+
+        contentContainer.appendChild(salesLineElement);
+      }
+    });
+  } catch (error) {
+    console.error("Error rendering sales lines data:", error);
+    contentContainer.innerHTML = `<p>Error loading sales lines: ${error.message}</p>`;
+  }
+}
+customersButton.addEventListener("click", renderCustomers);
 itemsButton.addEventListener("click", renderItems);
 salesQuotesButton.addEventListener("click", renderSalesQuotes);
